2c16233a241824c448340e91f8de37c3584adb42
evatutty <evaa7255@gmail.com>
2024-12-27 22:48:27 -0500

    variables can now be inputted

--- torque/sketch.js
+++ torque/sketch.js
@@ -1,15 +1,51 @@
 const barWidth = 15 //you can set this to whatever looks good
 pivot = 50 //this should be set by the user, but for testing it's how you can set the pivot position of the bar
 length = 200 //this should also be set by the user
+let mass = 1 //user defined
+let angularVelocity = 0
+let angularAcceleration = 0
+let angle = 0
+let forces = []
+
+let pivotInput, massInput, lengthInput, forcePositionInput, forceMagnitudeInput, addForceButton;
+
 
 function setup() {
     createCanvas(800, 800) //creating the canvas, 800 pixels by 800 pixels
+
+    createP("Length of bar:")
+    lengthInput = createInput(length.toString());
+    lengthInput.input(() => {
+        length = parseFloat(lengthInput.value()); //float makes sense right
+    });
+    
+    createP("Mass of bar:")
+    massInput = createInput(mass.toString());
+    massInput.input(() => {
+        mass = parseFloat(massInput.value());
+    });
+
+    createP("Position of pivot:")
+    pivotInput = createInput(pivot.toString());
+    pivotInput.input(() => {
+        pivot = parseFloat(pivotInput.value());
+    });
+
+    createP("Position of force on bar:");
+    forcePositionInput = createInput("100") //what
+
+    createP("Magnitude of force (y component)")
+    forceMagnitudeY = createInput("10") //also what
+
+   
 }
 
 let t = 0 //a variable to keep track of the time since the simulation started
 function draw() {
     background(51) //filling the background of the canvas with a solid color (i think this looks nice but you can do whatever with it)
     t += deltaTime / 1000 //p5 keeps track of deltaTime, which is the time between frames in milliseconds. adding this (converted to seconds) to our tally of the time keeps the time up to date
+
+    let barMomentOfInertia = (mass * length ** 2) / 3 //***assuming that I bar = 1/3mL^2?? but the pivot changes so doesnt that change mot as well???***
 
     // between push and pop, you can do things like translate, rotate, and scale, so that when you draw something it would be affected by those transformations
     // i find it makes most sense to read them bottom to top
@@ -24,8 +60,6 @@ function getTorque(radius, force) {
     return radius * force.y
 }
 
-let forces = []
-let pivot, mass, length, angularVelocity, angularAcceleration, angle
 
 function sumTorque() {
     let netTorque = 0
