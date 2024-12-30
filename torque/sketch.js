842793ef99cf738ebf743ce531c481fa3a62c212
evatutty <evaa7255@gmail.com>
2024-12-29 22:29:13 -0500

    draw stuff

--- torque/sketch.js
+++ torque/sketch.js
@@ -51,14 +51,42 @@ function draw() {
     background(51) //filling the background of the canvas with a solid color (i think this looks nice but you can do whatever with it)
     t += deltaTime / 1000 //p5 keeps track of deltaTime, which is the time between frames in milliseconds. adding this (converted to seconds) to our tally of the time keeps the time up to date
 
-    
+    let momentOfInertia = calculateMomentOfInertia(mass, length, pivot)
+
+    let netTorque = sumTorque(pivot)
+    angularAcceleration = netTorque / momentOfInertia // t = Ia
+    angularVelocity += angularAcceleration * t //??
+    angle += angularVelocity * t //???
     // between push and pop, you can do things like translate, rotate, and scale, so that when you draw something it would be affected by those transformations
     // i find it makes most sense to read them bottom to top
+
+    //bar
     push()
     translate(width / 2, height / 2) //finally it's moved to the center of the canvas: at a position with half the width and height of the canvas
     rotate(t) //it then rotates the rectangle. rotation always happens around the origin, that's why it's only being moved to the center of the canvas in the translate above
     rect(-pivot / length * 400, -barWidth / 2, 400, barWidth) //this one is kinda hard to explain, so i drew it out in "bar diagram.png"
     pop()
+
+    //pivot
+    push()
+    translate(width / 2, height / 2)
+    let pivotX = -pivot * (400/length) //?????
+    fill(255, 0 ,0) //red
+    ellipse(pivotX, 0, 10);
+    pop()
+
+    //forces
+    push()
+    translate(width / 2, height / 2)
+    for (let forces of forces) {
+        let posX = (force[0] - pivot) * (400 / length) //???
+        stroke(0, 255, 0) //green
+        strokeWeight(2)
+        line(posX, 0, posX, force [1].y * -10)
+    }
+    pop()
+
+
 }
 
 function getTorque(radius, force) {
@@ -79,10 +107,10 @@ function sumTorque() {
 }
 
 
-function calculateMomentOfInertia(mass, length, pivotPosition) {
+function calculateMomentOfInertia(mass, length, pivot) {
     let density = mass / length
-    let I1 = (pivotPosition**3)/3
-    let I2 = ((length**3)/3) - (p*(length**2)) + ((p**2)*L) - ((p**3)/3) //not sure ab order of operations on this one (embarassing)
+    let I1 = (pivot**3)/3
+    let I2 = (((length**3)/3) - (pivot*(length**2))) + (((pivot**2)*L) - ((pivot**3)/3))
     return density(I1 + I2)
 
 }
